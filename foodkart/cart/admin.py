fc7ceaf028091445a40e3bb708ed0776cf6bc747
Gaurav Dhanuka <gauravdhanuka2007@gmail.com>
2025-06-18 16:32:55 +0545

    adding admin view for Product Table

--- foodkart/cart/admin.py
+++ foodkart/cart/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import Product
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Product model in the Django admin.
+    """
+    list_display = ('name', 'price_per_kg', 'stock_in_kg')
+    search_fields = ('name',)
+    list_filter = ('stock_in_kg',)
+    ordering = ('name',)
